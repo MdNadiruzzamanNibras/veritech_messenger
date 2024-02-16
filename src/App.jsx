873a70cadb90132e5ce6ece2b03b0b4fc59d1192
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-02-16 18:23:19 +0600

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
+    <div className="container mx-auto">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <div className="absolute bottom-5 left-96">
+          <input type="text" className="w-96 h-10 bottom-0 rounded-full border border-gray-900" />
+        </div>
+     </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
